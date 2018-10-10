bf5099927a184cb38fe2c7838b2916a06886a2b6
TheRoom10 <44023769+TheRoom10@users.noreply.github.com>
2018-10-10 13:30:34 +0200

    Update img-lazy-load.js

--- img-lazy-load.js
+++ img-lazy-load.js
@@ -1,6 +1,8 @@
-[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
-    img.setAttribute('src', img.getAttribute('data-src'));
-    img.onload = function() {
-        img.removeAttribute('data-src');
-    };
-});
+function LazyLoad (){
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function() {
+            img.removeAttribute('data-src');
+        };
+    });
+}
